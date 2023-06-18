5f2ba13886f39b004ea38dbe63bdb2ef76008fec
Dries Meerman <driesmeerman@gmail.com>
2023-06-18 23:37:26 +0200

    add ability to turn off animations

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -1,43 +1,100 @@
 <script>
-    import {darkMode, toggleDarkMode} from "../state"
+    import { darkMode, toggleDarkMode, particlesEnabled, toggleParticles } from "../state";
 
     let darkModeIconFill;
-    let menuItems = [
-        {link: "#/skills", text: "Skills"},
-        {link: "#/experience", text: "Experience"},
-        {link: "#/education", text: "Education"},
-        {link: "#/blog", text: "Blog"},
-    ]
+    let particleText = "Disable particles";
 
-    darkMode.subscribe(enabled => {
-        if (enabled) darkModeIconFill = "white"
+    let menuItems = [
+        { link: "#/skills", text: "Skills" },
+        { link: "#/experience", text: "Experience" },
+        { link: "#/education", text: "Education" },
+        { link: "#/blog", text: "Blog" },
+    ];
+
+    darkMode.subscribe((enabled) => {
+        if (enabled) darkModeIconFill = "white";
         else darkModeIconFill = "black";
-    })
+    });
+
+    particlesEnabled.subscribe((enabled) => {
+        if (enabled) particleText = "Disable particles";
+        else particleText = "Enable particles";
+    });
 </script>
 
-<nav class="border-b-2 border-teal-500 dark:border-sky-600 dark:bg-zinc-700 flex items-center justify-between flex-wrap p-3 transition5">
+<nav
+    class="border-b-2 border-teal-500 dark:border-sky-600 dark:bg-zinc-700 flex items-center justify-between flex-wrap p-3 transition5"
+>
     <div class="flex items-center flex-shrink-0 dark:text-white mr-6">
         <a href="/#/" class="font-semibold text-xl tracking-tight">Meerman</a>
     </div>
 
-    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto lg:h-7">
+    <div
+        class="w-full block flex-grow lg:flex lg:items-center lg:w-auto lg:h-7"
+    >
         <div class="text-sm lg:flex-grow">
             {#each menuItems as item}
-                <a href="{item.link}"
-                   class="block mtop-6 mt-4 lg:inline-block lg:mt-0 text-teal-900 dark:text-white dark:hover:text-sky-600 mr-4 hover:text-teal-600">
+                <a
+                    href={item.link}
+                    class="block mtop-6 mt-4 lg:inline-block lg:mt-0 text-teal-900 dark:text-white dark:hover:text-sky-600 mr-4 hover:text-teal-600"
+                >
                     {item.text}
                 </a>
             {/each}
         </div>
+        <div class="mx-2" 
+        title={particleText}
+        role="button"
+        tabindex="0"
+            on:click={() => toggleParticles()}
+            on:keydown={(e) => {
+                if (e.key === "Enter" || e.key === " ") {
+                    toggleParticles();
+                    e.preventDefault();
+                }
+            }}
+        >
+            <svg
+                fill={darkModeIconFill}
+                width="24px"
+                height="24px"
+                viewBox="0 0 16 16"
+            >
+                <path
+                    d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828l.645-1.937zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.734 1.734 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.734 1.734 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.734 1.734 0 0 0 3.407 2.31l.387-1.162zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L10.863.1z"
+                />
+            </svg>
+        </div>
         <div>
-            <div tabindex="0" class="cursor-pointer mtop-6" role="button" on:click={toggleDarkMode} 
-            on:keydown={(e) => {if (e.key === 'Enter' || e.key === ' ') toggleDarkMode()}} 
-            title={$darkMode ? 'Dark' : 'Light'} >
-                <svg class="transition5" width="24px" height="24px" viewBox="0 0 24 24" fill="{darkModeIconFill}"
-                     xmlns="http://www.w3.org/2000/svg">
-                    <path d="M12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8V16Z"/>
-                    <path fill-rule="evenodd" clip-rule="evenodd"
-                          d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM12 4V8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16V20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4Z"/>
+            <div
+                tabindex="0"
+                class="cursor-pointer mtop-6"
+                role="button"
+                on:click={toggleDarkMode}
+                on:keydown={(e) => {
+                    if (e.key === "Enter" || e.key === " ") {
+                        toggleDarkMode();
+                        e.preventDefault();
+                    }
+                }}
+                title={$darkMode ? "Dark" : "Light"}
+            >
+                <svg
+                    class="transition5"
+                    width="24px"
+                    height="24px"
+                    viewBox="0 0 24 24"
+                    fill={darkModeIconFill}
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        d="M12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8V16Z"
+                    />
+                    <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM12 4V8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16V20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4Z"
+                    />
                 </svg>
             </div>
         </div>
