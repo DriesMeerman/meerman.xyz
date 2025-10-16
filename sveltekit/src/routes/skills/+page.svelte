935970d8a9c0610570053a25ce167766adfb5b00
Dries Meerman <driesmeerman@gmail.com>
2025-10-17 01:40:12 +0200

    Fix skill page

--- sveltekit/src/routes/skills/+page.svelte
+++ sveltekit/src/routes/skills/+page.svelte
@@ -11,33 +11,35 @@
     misc: { name: 'Miscellaneous', selected: true, items: skills.misc }
   });
 
-  let selectedCount = Object.values(categories).filter((c) => c.selected).length;
+  let selectedCount = $derived(Object.values(categories).filter((c) => c.selected).length);
   let keywords = Object.values(categories).flatMap((c) => c.items).map((s) => s.name).join(', ');
 </script>
 
 <div class="category-selectors flex flex-row flex-wrap mb-4">
   {#each Object.values(categories) as category}
     <div class="m-1">
-      <PillButton name={category.name} selected={category.selected} onclick={() => { category.selected = !category.selected; selectedCount += category.selected ? 1 : -1; }} />
+      <PillButton name={category.name} selected={category.selected} on:click={() => { category.selected = !category.selected; }} />
     </div>
   {/each}
   </div>
 
-<Card class="px-2 py-3 mb-4 flex flex-col backdrop-blur-sm {selectedCount ? '' : 'hidden'} ">
-  {#each Object.values(categories) as category}
-    <div class="p-1" class:hidden={!category.selected}>
-      <h4 class="h4 ml-2">{category.name}</h4>
-      <hr />
-      <div class="flex flex-row justify-center flex-wrap md:p-4 md:mx-8  gap-4 md:gap-8 py-4">
-        {#each category.items || [] as skill}
-          <div class="scale-90 md:scale-100">
-            <SkillCard {skill} />
-          </div>
-        {/each}
+{#if selectedCount > 0}
+  <Card class="px-2 py-3 mb-4 flex flex-col backdrop-blur-sm">
+    {#each Object.values(categories) as category}
+      <div class="p-1" class:hidden={!category.selected}>
+        <h4 class="h4 ml-2">{category.name}</h4>
+        <hr />
+        <div class="flex flex-row justify-center flex-wrap md:p-4 md:mx-8  gap-4 md:gap-8 py-4">
+          {#each category.items || [] as skill}
+            <div class="scale-90 md:scale-100">
+              <SkillCard {skill} />
+            </div>
+          {/each}
+        </div>
       </div>
-    </div>
-  {/each}
-</Card>
+    {/each}
+  </Card>
+{/if}
 
 <svelte:head>
   <title>Meerman</title>
--- sveltekit/static/build/bundle.js
+++ sveltekit/static/build/bundle.js
@@ -11006,7 +11006,7 @@ var app = (function () {
 		}
 	}
 
-	// Generated at 2025-08-10T08:21:34.434Z
+	// Generated at 2025-10-16T22:16:03.914Z
 	const articlesString = `[
   {
     "filename": "dr-001.md",
@@ -11020,7 +11020,7 @@ var app = (function () {
     "date": "2023-05-28T00:00:00.000Z",
     "author": "Dries Meerman",
     "ID": 1,
-    "url": "https://meerman.xyz/blog/dr-001"
+    "url": "https://meerman.xyz/#/blog/dr-001"
   },
   {
     "filename": "dr-002.md",
@@ -11032,7 +11032,7 @@ var app = (function () {
     "date": "2023-09-25T00:00:00.000Z",
     "author": "Dries Meerman",
     "ID": 2,
-    "url": "https://meerman.xyz/blog/dr-002"
+    "url": "https://meerman.xyz/#/blog/dr-002"
   },
   {
     "filename": "dr-003.md",
@@ -11047,7 +11047,7 @@ var app = (function () {
     "author": "Dries Meerman",
     "ID": 3,
     "summary": "The blog post titled 'Hybrid Automation' explores a practical approach to combining automation and manual tasks to achieve optimal efficiency. The author introduces the concept of hybrid automation, where automation is applied to tasks that are easily automated, and manual tasks are retained if they are not too tedious and don't benefit significantly from automation. The post delves into the Pareto Principle, commonly known as the 80/20 rule, where 80% of outcomes come from 20% of causes, emphasizing the efficient use of resources. The author provides a real-life example involving the automated downloading of images from a website and explains how they leveraged the ChatGPT tool to simplify the process. They describe the creation of scripts for obtaining image links, copying the data to the clipboard, and a bash script for downloading the images. The post concludes by highlighting the benefits of breaking tasks into manageable components that can be easily automated or manually executed to bridge gaps effectively.",
-    "url": "https://meerman.xyz/blog/dr-003"
+    "url": "https://meerman.xyz/#/blog/dr-003"
   },
   {
     "filename": "dr-004.md",
@@ -11059,7 +11059,7 @@ var app = (function () {
     "author": "Dries Meerman",
     "summary": "Swift 5.9, released with Xcode 15.2, introduces significant language improvements, including bi-directional C++ compatibility, macros, and the use of 'if' and 'switch' as expressions for more readable code. Debugging sees a speed boost, particularly in 'p' and 'po' commands, and there are notable enhancements to the Swift Package Manager and the Swift-syntax project. The author is particularly excited about the potential for cleaner code with 'if' expressions and the anticipated improvements in code analysis tools. This update also brings advancements for Swift on Windows platforms.",
     "ID": 4,
-    "url": "https://meerman.xyz/blog/dr-004"
+    "url": "https://meerman.xyz/#/blog/dr-004"
   },
   {
     "filename": "dr-005.md",
@@ -11075,7 +11075,7 @@ var app = (function () {
     "author": "Dries Meerman",
     "summary": "The article discusses the Dries' experience with using AI-enhanced IDEs, specifically Cursor, to address issues on their website, including markdown flavour inconsistencies and large bundle sizes. Initially hesitant to fix these problems due to their complexity and existing functionality, the author explores how AI can assist in making the process faster and less tedious. They delve into the complexities of markdown variations and their desire to implement footnotes, eventually using AI to optimize the site's structure and reduce bundle sizes. While the AI helped streamline tasks, it also introduced some minor bugs, such as incorrect file capitalization, highlighting both the potential and limitations of AI-driven development. Overall, the author found AI tools useful for problem-solving and workflow improvements, despite occasional challenges.",
     "ID": "005",
-    "url": "https://meerman.xyz/blog/dr-005"
+    "url": "https://meerman.xyz/#/blog/dr-005"
   },
   {
     "filename": "dr-006.md",
@@ -11089,7 +11089,19 @@ var app = (function () {
     "author": "Dries Meerman",
     "ID": "006",
     "summary": "Inspired by recent advancements in agentic AI like VS Code's agent mode and Google's Agent2Agent protocol, the author explores the Model Context Protocol (MCP), an open standard allowing Large Language Models (LLMs) to request external context and utilize tools for more accurate, less hallucinatory results. Motivated to apply this to their ServiceNow expertise, the author developed a custom MCP server (available on GitHub) to retrieve table schemas, detailing the building process using official guides and the MCP Inspector tool for local testing. The article concludes by demonstrating the successful integration of this server into the Cursor editor, where an LLM agent uses the custom tool to fetch live ServiceNow data and generate code based on it, showcasing MCP's potential to enhance AI coding assistants with reliable, real-world context and actions.",
-    "url": "https://meerman.xyz/blog/dr-006"
+    "url": "https://meerman.xyz/#/blog/dr-006"
+  },
+  {
+    "filename": "dr-007.md",
+    "title": "30 bits of unsolicited advice",
+    "tags": [
+      "advice"
+    ],
+    "date": "2025-10-17T00:00:00.000Z",
+    "author": "Dries Meerman",
+    "ID": "007",
+    "summary": "This blog post, written upon turning 30, is a reflection on lessons learned through trial, curiosity, and self-discovery in one’s 20s. It’s not a list of rigid rules, but a collection of practical and philosophical reminders—about acting instead of overthinking, creating without fear, nurturing curiosity, building habits that last, and embracing both the weird and the ordinary. It’s about realizing that growth comes from showing up, experimenting, and engaging with life, while learning to balance reflection with action.",
+    "url": "https://meerman.xyz/#/blog/dr-007"
   }
 ]`;
 	const articles = JSON.parse(articlesString);
