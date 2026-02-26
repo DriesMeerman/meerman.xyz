4512b0092a4bdf617923690d3edb461a16582ce5
Dries Meerman <driesmeerman@gmail.com>
2026-02-26 22:44:59 +0100

    Add cats to cat page

---
+++ src/routes/cats/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+    let cats = [
+        {
+            name: 'Henrietta',
+            image: '/assets/cats/Fenry.jpeg'
+        },
+        {
+            name: 'Gina',
+            image: '/assets/cats/Gina.jpeg'
+        }
+    ]
+</script>
+
+<div class="cats-page">
+  <div class="cat-container display-flex flex flex-wrap gap-12">
+    {#each cats as cat}
+      <div class="cat-card ">
+        <h2 class="text-xl font-bold text-center my-2">{cat.name}</h2>
+        <img src={cat.image} alt={cat.name} class="w-full h-full object-cover rounded-full w-64 h-64 overflow-hidden" />
+      </div>
+    {/each}
+  </div>
+</div>
+
+<style>
+  .cats-page {
+    display: grid;
+    gap: 1rem;
+  }
+</style>
